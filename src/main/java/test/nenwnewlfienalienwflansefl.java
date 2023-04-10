22a15ec265a7e662ae1c76d951a9b8cab438ea4b
SkylarLee1003 <skylarlee1003@gmail.com>
2023-04-10 18:45:17 +0900

    seoyeonnew

---
+++ src/main/java/test/nenwnewlfienalienwflansefl.java
@@ -0,0 +1,8 @@
+package test;
+
+public class nenwnewlfienalienwflansefl {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("newnenwleii");
+	}
+}
